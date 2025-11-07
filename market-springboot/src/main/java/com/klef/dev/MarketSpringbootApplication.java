46ef917b9351194f713ffcbee6ec1939642aa404
156 <2300030156@kluniversity.in>
2025-11-07 06:35:12 +0530

    updated

--- market-springboot/src/main/java/com/klef/dev/MarketSpringbootApplication.java
+++ market-springboot/src/main/java/com/klef/dev/MarketSpringbootApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MarketSpringbootApplication extends SpringBootServletInitializer {
+public class MarketSpringbootApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(MarketSpringbootApplication.class, args);
